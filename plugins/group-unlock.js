34c823a2a4d9a677b251d2c94ac1ab2c1c0cdaac
𝗡𝗢𝗩𝗔-𝗫𝗠𝗗 <hanchobmb@gmail.com>
2025-08-09 16:58:45 +0300

    Update group-unlock.js

--- plugins/group-unlock.js
+++ plugins/group-unlock.js
@@ -3,7 +3,7 @@ const { cmd, commands } = require('../command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 
 cmd({
-    pattern: "unlockgc",
+    pattern: "unlock",
     alias: ["unlock"],
     react: "🔓",
     desc: "Unlock the group (Allows new members to join).",
